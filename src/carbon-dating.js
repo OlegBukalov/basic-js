460f1e6d1bf00d135d7d3882434f256c08a7d0f6
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2020-02-23 23:55:53 +0300

    Add code for carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,14 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+  if (typeof(n) != "string" || n.trim() == "") {
+    return false;
+  }
+  if (isNaN(n)) {
+      return false;
+  }
+  n = Number(n);
+  let rez = 0, HALF_LIFE_PERIOD = 5730, MODERN_ACTIVITY = 0.693, k;
+  k = MODERN_ACTIVITY / HALF_LIFE_PERIOD;
+  return Math.round(Math.log(15/n)/k);
 };
