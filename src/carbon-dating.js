a93e396e89b879f78560157e1e1782d5ed6fe282
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2020-02-24 22:32:54 +0300

    bugs fixed carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,15 +1,21 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(n) {
   if (typeof(n) != "string" || n.trim() == "") {
     return false;
   }
+  if (n.match(/[\d]+[.][\d]+[.]/)) {
+    n = n.slice(0,n.lastIndexOf("."))+n.slice(n.lastIndexOf(".")+1,n.length-1);
+  }
   if (isNaN(n)) {
-      return false;
+    return false;
+  }
+  if (n>15 || n <= 0) {
+    return false;
   }
   n = Number(n);
-  let rez = 0, HALF_LIFE_PERIOD = 5730, MODERN_ACTIVITY = 0.693, k;
-  k = MODERN_ACTIVITY / HALF_LIFE_PERIOD;
-  return Math.round(Math.log(15/n)/k);
+  let rez = 0, k;
+  k = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY/n)/k);
 };
