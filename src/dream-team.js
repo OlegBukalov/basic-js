9dd29ba20ce716bb8190d88d461326db5870723d
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2020-02-26 17:21:38 +0200

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if (arr == null || arr == undefined) {
+    return false;
+  }
+  let dreamTeam = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] == "string") {
+      let bigStr = arr[i].trim().toUpperCase();
+      dreamTeam.push(bigStr[0]);
+    }
+  }
+  return dreamTeam.length > 0 ? dreamTeam.sort().join('') : false;
 };
