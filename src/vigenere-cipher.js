8a20bba556e88472af4a2f9dd1a5f85bada3cc41
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2020-02-26 16:28:07 +0200

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,41 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    
+    constructor(mode) {
+        this.mode = mode != undefined ? mode : true;
+    }
+    
+    encrypt(str, key) {
+        
+        str = str.toUpperCase();
+        key = key.toUpperCase();
+        let result = '', j=0;
+
+        for (let i = 0; i < str.length; i++) {
+            let keySymb = key[(j>=key.length ? j%key.length : j)];
+            if (str[i].charCodeAt() > 64 && str[i].charCodeAt() < 91) {
+                result += String.fromCharCode((str[i].charCodeAt() + keySymb.charCodeAt() + 26) % 26 +65);
+                j++;
+            } else
+                result += str[i]
+        }
+        return this.mode ? result : result.split('').reverse().join('');
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(str, key) {
+        
+        str = str.toUpperCase();
+        key = key.toUpperCase();
+        let result = '', j=0;
+
+        for (let i = 0; i < str.length; i++) {
+            let keySymb = key[(j>=key.length ? j%key.length : j)];
+            if (str[i].charCodeAt() > 64 && str[i].charCodeAt() < 91) {
+                result += String.fromCharCode((str[i].charCodeAt() - keySymb.charCodeAt() + 26) % 26 +65);
+                j++;
+            } else
+                result += str[i]
+        }
+        return this.mode ? result : result.split('').reverse().join('');
     }
 }
 
