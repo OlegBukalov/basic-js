ba7e3e063a18d800e34f9d8effd25149bf57b317
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2020-02-28 14:54:23 +0200

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,23 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error('Error');
+    };
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '--discard-next') {
+            i++;
+        } else if (arr[i] == '--discard-prev') {
+            if (i != 0)
+                newArr.splice(newArr.length-1, 1);
+        } else if (arr[i] == '--double-next') {
+            if (i+1 != arr.length)
+                newArr.push(arr[i + 1]);
+        } else if (arr[i] == '--double-prev') {
+            if (i != 0)
+                newArr.push(arr[i - 1]);
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
