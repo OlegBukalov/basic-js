0e4939cc3f6b202ae3096a8aa6ae932ba09a17bd
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2020-03-01 02:05:15 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,32 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let repeatTimes=1, newStr='', separator='+', addition='', additionRepeatTimes=1, additionSeparator='|';
+    if (options.repeatTimes != undefined) {
+        repeatTimes = options.repeatTimes;
+    }
+    if (options.repeatTimes != undefined) {
+        additionRepeatTimes = options.additionRepeatTimes;
+    }
+    if (options.separator != undefined) {
+        separator = options.separator;
+    }
+    if (options.addition !== undefined) {
+        addition = options.addition;
+    }
+    if (options.additionSeparator != undefined) {
+        additionSeparator = options.additionSeparator;
+    }
+    for (let i = 0; i < repeatTimes; i++) {
+        newStr += str;
+        for (let j = 0; j < additionRepeatTimes; j++) {
+            newStr += addition + additionSeparator;
+            if (j+1 == additionRepeatTimes) {
+                newStr = newStr.substr(0, newStr.length-additionSeparator.length);
+            }
+        }
+        newStr += separator;
+        if (i+1 == repeatTimes) {
+            newStr = newStr.substr(0, newStr.length-separator.length);
+        }
+    }
+    return newStr;
 };
-  
